dce310fc77791db3f6a5233ccd0d9322dad8b9b1
Naini-magic <nraghuwanshi00401@gmail.com>
2025-07-10 22:56:13 +0530

    sign/login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
 import Navbar from './components/Home/Navbar';
 import HomePage from './components/Home/HomePage';
+import Signup from './components/Auth/Signup';
+import Login from './components/Auth/Login';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
