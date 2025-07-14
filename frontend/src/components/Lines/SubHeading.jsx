9b5cbe1f5a92f856c2330457156924436eb9442b
Naini-magic <nraghuwanshi00401@gmail.com>
2025-07-14 18:48:07 +0530

    14_july

--- frontend/src/components/Lines/SubHeading.jsx
+++ frontend/src/components/Lines/SubHeading.jsx
@@ -5,9 +5,9 @@ const SubHeadImg = ({ text }) => {
   return (
     <div className="inline-flex flex-col items-center relative mb-2">
       {/* Heading Text */}
-      <h3 className="text-2xl md:text-[42px] font-medium text-transparent bg-clip-text bg-gradient-to-r from-[#1D2026] via-[#e45c02] to-[#FF6501]">
+      {/* <h3 className="text-2xl md:text-[42px] font-medium text-transparent bg-clip-text bg-gradient-to-r from-[#1D2026] via-[#e45c02] to-[#FF6501]">
         {text}
-      </h3>
+      </h3> */}
 
       <h3 className="text-2xl md:text-[42px] font-medium bg-clip-text text-transparent bg-gradient-to-r from-[#1D2026] via-[#e45c02] to-[#FF6501] bg-[length:400%_auto] animate-gradient">
         {text}
