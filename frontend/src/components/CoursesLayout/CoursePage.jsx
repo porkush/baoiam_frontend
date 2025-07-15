0ea02e7a3df20c4189bef7e8857c2c595b4c7d9e
Naini-magic <nraghuwanshi00401@gmail.com>
2025-07-15 11:25:03 +0530

    15_july

--- frontend/src/components/CoursesLayout/CoursePage.jsx
+++ frontend/src/components/CoursesLayout/CoursePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import HeroSection_C from './HeroSection_C'
+import Footer from "../Footer/Footer"
+import FAQ_C from './FAQ_C'
+import DiscountBanner from './DiscountBanner'
 
 const CoursePage = () => {
   return (
     <div>
       <HeroSection_C/>
+      <FAQ_C/>
+      <DiscountBanner/>
+      <Footer/>
     </div>
   )
 }
 
-export default CoursePage
+export default CoursePage;
